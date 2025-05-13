59e4832a0910bf30a790c97460a90ff49194064f
bernrd25 <bernardo.ochoa@concentrix.com>
2025-05-13 16:19:46 +0800

    feat: pages and sidebar

---
+++ src/react/components/AppSidebar.tsx
@@ -0,0 +1,67 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./Sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/embedded-survey-playground/apps",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "/embedded-survey-playground/apps/inbox",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "/embedded-survey-playground/apps/calendar",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "/embedded-survey-playground/apps/search",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "/embedded-survey-playground/apps/settings",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
